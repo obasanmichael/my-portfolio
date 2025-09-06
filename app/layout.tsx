4599d2b48e5b39ea4b47631472451015fc90c3bd
Obasan Tolulope <obasantolu@gmail.com>
2025-09-06 19:58:25 +0100

    making ui changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Tolu's Portfolio",
+  description: "Building Sleek and Modern Web Apps",
 };
 
 export default function RootLayout({
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,11 +23,7 @@ const Hero = () => {
         <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
       </div>
 
-      {/**
-       *  UI: grid
-       *  change bg color to bg-black-100 and reduce grid color from
-       *  0.2 to 0.03
-       */}
+    
       <div
         className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
        absolute top-0 left-0 flex items-center justify-center"
